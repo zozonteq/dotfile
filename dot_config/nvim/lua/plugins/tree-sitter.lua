4699074eadff4bc4b99ac3c7cd3ae32a77190ce5
oto <ottotak@outlook.com>
2024-10-23 08:51:24 +0900

    edit treesitter

--- dot_config/nvim/lua/plugins/tree-sitter.lua
+++ dot_config/nvim/lua/plugins/tree-sitter.lua
@@ -3,12 +3,14 @@ return {
   config = function()
     require"nvim-treesitter.configs".setup{
       ensure_installed = {
-        "http", "json"
+        "http", "json", "tsx", "yaml", "css", "lua", "php", "toml"
       },
       highlight = {
         enable = true,
       }
     }
+    local parse_config = require"nvim-treesitter.parsers".get_parser_configs()
+    parse_config.tsx.filetype_to_parsename = {"javascript", "typescript.tsx"}
   end
 
 }
