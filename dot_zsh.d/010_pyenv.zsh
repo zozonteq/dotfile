e371df9fa013bd4e1fa3725bbd1351ec28e8d34f
tak <ottotak@outlook.com>
2024-10-26 23:25:12 +0900

    do not setup if pyenv not found

--- dot_zsh.d/010_pyenv.zsh
+++ dot_zsh.d/010_pyenv.zsh
@@ -1,4 +1,6 @@
-
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+if command -v pyenv 2>&1 >/dev/null
+then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
+fi
