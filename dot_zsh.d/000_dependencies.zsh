3fa8925e20fb94231a206bf8abcdd79cc2daceda
oto <ottotak@outlook.com>
2024-10-31 00:14:29 +0900

    zsh completions

--- dot_zsh.d/000_dependencies.zsh
+++ dot_zsh.d/000_dependencies.zsh
@@ -2,6 +2,11 @@ export UNAME="$(uname)"
 export PATH=$PATH:~/.bin
 export XDG_CONFIG_HOME="$HOME/.config"
 
+
+autoload -Uz compinit && compinit
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*:default' menu select=1 
+
 # install sheldon
 [ ! -f ~/.bin/sheldon ] &&curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
       | bash -s -- --repo rossmacarthur/sheldon --to ~/.bin
