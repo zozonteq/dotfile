0bd17c04616b07d61f4772d6baffd9e93639329e
oto <ottotak@outlook.com>
2024-10-31 18:12:54 +0900

    zsh comp color

--- dot_config/nvim/readme.md
+++ dot_config/nvim/readme.md
@@ -1,2 +1,5 @@
 # neovim
-My neovim config written in lua with lazy plugin manager.
+My neovim config written in lua with `lazy` plugin manager.
+
+# plugins
+see `./lua/plugins/`
--- dot_zsh.d/000_dependencies.zsh
+++ dot_zsh.d/000_dependencies.zsh
@@ -4,6 +4,12 @@ export XDG_CONFIG_HOME="$HOME/.config"
 
 
 autoload -Uz compinit && compinit
+
+autoload colors
+colors
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*:default' menu select=1 
 
